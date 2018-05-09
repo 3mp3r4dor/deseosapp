7d5c5134d1c83705f80b56cba3d088a7f00b0c4c
Julio Obregon <jc_ingenieria_ti@hotmail.com>
2018-05-08 22:26:01 -0500

    agregar lista

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { Lista, ListaItem } from '../../app/classes/index';
+import { AlertController, NavController } from 'ionic-angular';
+
+
+//servicios
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
+
 @Component({
     selector: 'app-agregar',
     templateUrl: 'agregar.component.html',
@@ -7,12 +13,14 @@ import { Lista, ListaItem } from '../../app/classes/index';
 
 export class AgregarComponent implements OnInit {
 
-    nombreLista: string;
+    nombreLista: string = "";
     nombreItem: string = "";
 
     items: ListaItem[] = [];
 
-    constructor( ) {}
+    constructor( public _listaDeseos: ListaDeseosService, 
+                 public alertController: AlertController,
+                 public navController: NavController) {}
 
     ngOnInit() {}
 
@@ -27,6 +35,29 @@ export class AgregarComponent implements OnInit {
 
         this.items.push( item );
         this.nombreItem = "";
+    }
+
+    borrar( idx: number ) {
+        this.items.splice( idx, 1);
+    }
+
+    guardarLista() {
+        if( this.nombreLista.length == 0 ){
+            let alert = this.alertController.create({
+                title: 'Nombre de la lista',
+                subTitle: 'El nombre de la lista es necesaria!',
+                buttons: ['OK']
+            });
+            alert.present();
+            return;            
+        }
+
+        let lista = new Lista( this.nombreLista );
+        lista.items = this.items;
+
+        this._listaDeseos.listas.push( lista );
+        this.navController.pop();
+
     }
     
 } 
